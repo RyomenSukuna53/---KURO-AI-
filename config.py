459e1de33c758413bf38d4c173d0a4077bc61302
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-09 09:00:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os 
+from os import getenv
+
+API_ID = int(getenv("API_ID", 27548865)) 
+API_HASH = getenv("API_HASH", "db07e06a5eb288c706d4df697b71ab61") 
+TOKEN = getenv("TOKEN", "7821914037:AAFdl2jEUdJmHNpA32rbEnuOhVvAMa-NNiI") 
+OWNER_ID = int(getenv(" OWNER_ID", 6239769036)) 
