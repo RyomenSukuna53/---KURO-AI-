b5de7b06021715a840d5a71b1b0bcddb638893b3
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-10 17:28:53 +0530

    Update auth.py

--- KuroAI/KUROMAIN/COMMAND/auth.py
+++ KuroAI/KUROMAIN/COMMAND/auth.py
@@ -12,24 +12,16 @@ from KuroAI import HANDLERS
 @bot.on_message(filters.command(["authorize", "auth"], prefixes=HANDLERS) & filters.user(OWNER_ID))
 async def authorize_user(client, message):
     if not message.reply_to_message:
-        msg = await message.reply(
-        f"```shell\n[𝗞𝗨𝗥𝗢-𝗫𝗔𝗜] ==> Authorizing {user_id}...\n{bars[0]}```",
-        parse_mode=ParseMode.MARKDOWN
-    )
-
-    for bar in bars[1:]:
-        await asyncio.sleep(0.2)
-        await msg.edit_text(f"```shell\n[𝗞𝗨𝗥𝗢-𝗫𝗔𝗜] ==> Authorizing {user_id}...\n{bar}```", parse_mode=ParseMode.MARKDOWN)
-
-    auth_col.insert_one({"_id": user_id})
-    await msg.edit_text(f"```shell\n[𝗞𝗨𝗥𝗢-𝗫𝗔𝗜] ==> {user_id} Authorized✅\n{bars[-1]}```", parse_mode=ParseMode.MARKDOWN)
-
+        await message.reply(" reply to a user") 
+        return 
+       
+    
 
 
     user = message.reply_to_message.from_user
     user_id = user.id
 
-    if not auth_col.find_one({"_id": user_id}):
+    if auth_col.find_one({"_id": user_id}):
         return await message.reply("✅ This user is already authorized.")
     
     if ban_col.find_one({"_id": user_id}):
@@ -43,7 +35,17 @@ async def authorize_user(client, message):
     ]
 
 
-    await msg.reply("USER AUTHORIZED") 
-    return 
+    msg = await message.reply(
+        f"```shell\n[𝗞𝗨𝗥𝗢-𝗫𝗔𝗜] ==> Authorizing {user_id}...\n{bars[0]}```",
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+    for bar in bars[1:]:
+        await asyncio.sleep(0.2)
+        await msg.edit_text(f"```shell\n[𝗞𝗨𝗥𝗢-𝗫𝗔𝗜] ==> Authorizing {user_id}...\n{bar}```", parse_mode=ParseMode.MARKDOWN)
+
+    auth_col.insert_one({"_id": user_id})
+    await msg.edit_text(f"```shell\n[𝗞𝗨𝗥𝗢-𝗫𝗔𝗜] ==> {user_id} Authorized✅\n{bars[-1]}```", parse_mode=ParseMode.MARKDOWN)
+
 
 
