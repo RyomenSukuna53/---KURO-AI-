a377458fba2f671e7b9fe630dcd87d4fdbd9fe29
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-10 16:43:17 +0530

    Update __main__.py

--- KuroAI/__main__.py
+++ KuroAI/__main__.py
@@ -3,10 +3,7 @@ from pyrogram import Client, filters
 from KuroAI.KUROMAIN import * 
 import asyncio 
 from KuroAI import *
-from KuroAI.KUROMAIN.COMMAND import start, order
-from KuroAI.KUROMAIN.HELPERS import * 
-from KuroAI.KUROMAIN.OWNER_WORKS import auth, unauth, eval
-from KuroAI import logs, gpt
+from KurooAI import logs, gpt
 
 if __name__=="__main__":
     KuroAI.run()
