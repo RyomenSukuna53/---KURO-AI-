d3ec959eb5bcdb95358e2b35b3a999256a188cda
HG BOTS <92435453+hemanth233739@users.noreply.github.com>
2025-04-10 16:59:15 +0530

    Update logs.py

--- KuroAI/logs.py
+++ KuroAI/logs.py
@@ -1,38 +1,43 @@
 import io
-from pyrogram import *
-from KuroAI.__init__ import KuroAI as bot
-from KuroAI import HANDLERS
-from config import OWNER_ID
-from config import SUDO_USERS
 import traceback
 from subprocess import getoutput as run
+
+from pyrogram import filters
 from pyrogram.enums import ChatAction
+from pyrogram.types import Message
+
+from KuroAI.__init__ import KuroAI as bot
+from KuroAI import HANDLERS
+from config import OWNER_ID, SUDO_USERS
+
+# Combine OWNER_ID and SUDO_USERS into one set for checking
+AUTHORIZED_USERS = set(SUDO_USERS)
+AUTHORIZED_USERS.add(OWNER_ID)
 
 @bot.on_message(filters.command(["logs", "log"], prefixes=HANDLERS))
-async def logs(_, message):
-    if message.from_user.id == OWNER_ID or message.from_user.id in SUDO_USERS_ID:
-        print("")
-    else:
+async def tail_logs(_, message: Message):
+    if message.from_user.id not in AUTHORIZED_USERS:
         return
-    run_logs = run("tail log.txt")
-    text = await message.reply_text("`Getting logs...`")
-    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-    await message.reply_text(f"```shell\n{run_logs}```")
-    await text.delete()
-
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+        run_logs = run("tail log.txt")
+        text = await message.reply_text("`Getting logs...`")
+        await message.reply_text(f"```shell\n{run_logs}```")
+        await text.delete()
+    except Exception as e:
+        await message.reply_text(f"Error:\n{traceback.format_exc()}")
 
 @bot.on_message(filters.command(["flogs", "flog"], prefixes=HANDLERS))
-async def logs(_, message):
-    if message.from_user.id == OWNER_ID or message.from_user.id in SUDO_USERS_ID:
-        print("")
-    else:
+async def full_logs(_, message: Message):
+    if message.from_user.id not in AUTHORIZED_USERS:
         return
-    run_logs = run("cat log.txt")
-    text = await message.reply_text("`Sending Full logs...`")
-    await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
-    with io.BytesIO(str.encode(run_logs)) as logs:
-        logs.name = "log.txt"
-        await message.reply_document(
-            document=logs,
-        )
-    await text.delete()
+    try:
+        await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
+        run_logs = run("cat log.txt")
+        text = await message.reply_text("`Sending full logs...`")
+        with io.BytesIO(str.encode(run_logs)) as logs:
+            logs.name = "log.txt"
+            await message.reply_document(document=logs)
+        await text.delete()
+    except Exception as e:
+        await message.reply_text(f"Error:\n{traceback.format_exc()}")
