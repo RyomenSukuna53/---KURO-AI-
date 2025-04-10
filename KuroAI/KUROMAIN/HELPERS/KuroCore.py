a6f0a84c1ad62b69aec03d0a564172ffa7ce9105
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-10 10:43:48 +0530

    Add files via upload

---
+++ KuroAI/KUROMAIN/HELPERS/KuroCore.py
@@ -0,0 +1,37 @@
+import aiohttp
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from KURO-AI import KuroAI as app
+from config import OWNER_ID
+from KURO-AI import HANDLER
+from datetime import datetime
+import json
+
+MY_VERSION = 1.0
+
+async def fetch_data(query: str, message: Message) -> str:
+    try:
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Content-Type": "application/json"
+        }
+        url = "https://api.binjie.fun/api/generateStream"
+        data = {
+            "prompt": query,
+            "userId": f"#/chat/{message.from_user.id}",
+            "network": True,
+            "stream": False,
+            "system": {
+                "userId": "#/chat/1722576084617",
+                "withoutContext": False
+            }
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, json=data) as response:
+                return await response.text()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+
