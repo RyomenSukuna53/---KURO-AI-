e1abe752e55327733a5f78d54dde98ba41101905
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-10 10:51:30 +0530

    Update auth.py

--- KuroAI/KUROMAIN/OWNER_WORKS/auth.py
+++ KuroAI/KUROMAIN/OWNER_WORKS/auth.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.enums import ParseMode
 from KuroAI import KuroAI as bot
-from KuroAI.KURO-MAIN.HELPERS.auth import *
+from KuroAI.KUROMAIN.HELPERS.auth import *
 from config import *
 from KuroAI import HANDLERS
 import asyncio
