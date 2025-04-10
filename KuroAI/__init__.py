cc903b16554b720921444a788319d51fb49ea2f7
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-10 09:59:03 +0530

    Add files via upload

--- KuroAI/__init__.py
+++ KuroAI/__init__.py
@@ -1,1 +1,30 @@
+#AL things imported
+from config import * 
+from pyrogram import Client, filters 
+import os 
+import sys 
+import logging
 
+# LOGGING
+logging.basicConfig(
+  format="[KuroAI-Beta] %(name)s - %(levelname)s - %(message)s",
+  handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+  level=logging.INFO,
+)
+
+HANDLERS = [".", "!", "/", "#", "$", "%", "&", "*", "?"]
+
+
+#IF USER DON'T GET ANY OF THESE THINGS IN ENVIRONNEMENT
+if not (API_ID, API_HASH, TOKEN):
+  raise "Varaibles not found"
+
+#CREATING A CLIENT
+KuroAI = Client(
+  "KuroAI-COLXproMain", 
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+    plugins=dict(root="/KURO-AI/KURO-MAIN/COMMAND") 
+  
+)
---
+++ KuroAI/__main__.py
@@ -0,0 +1,12 @@
+#MAIN FILE THAT RUN ALL BOTS COMMANDS ETC 
+from pyrogram import Client, filters 
+from KURO-MAIN import * 
+import asyncio 
+from KURO-AI import *
+
+
+if __name__=="__main__":
+	KuroAI.run()
+	with KuroAI:
+		KuroAI.send_message(chat_id=6239769036, 
+												text="BOT STARTED MASTER") 
