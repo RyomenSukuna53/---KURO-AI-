beab7949f2c7682c851977210e4e0c27f5f58f8d
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-10 15:33:25 +0530

    Update gpt.py

--- KuroAI/KUROMAIN/COMMAND/gpt.py
+++ KuroAI/KUROMAIN/COMMAND/gpt.py
@@ -5,13 +5,39 @@ from KuroAI.__init__ import KuroAI as app
 from config import OWNER_ID
 from KuroAI import HANDLERS
 from datetime import datetime
-import json
-from KuroAI.KUROMAIN.HELPERS import KuroCore
-from KuroAI.KUROMAIN.HELPERS import * 
+import json 
 
 
 
 MY_VERSION = 1.0
+
+async def fetch_data(query: str, message: Message) -> str:
+    try:
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Content-Type": "application/json"
+        }
+        url = "https://api.binjie.fun/api/generateStream"
+        data = {
+            "prompt": query,
+            "userId": f"#/chat/{message.from_user.id}",
+            "network": True,
+            "stream": False,
+            "system": {
+                "userId": "#/chat/1722576084617",
+                "withoutContext": False
+            }
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, json=data) as response:
+                return await response.text()
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+        
+
+
 
 @app.on_message(filters.command(["KuroAI", "RaijinAI"], prefixes=HANDLERS))
 async def chatgpt(_: Client, message: Message):
