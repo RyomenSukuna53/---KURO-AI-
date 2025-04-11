74fe43d708f9f1e4b52d723095497434cd9721f5
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-11 16:13:39 +0530

    Update clear.py

--- KuroAI/KUROMAIN/COMMAND/clear.py
+++ KuroAI/KUROMAIN/COMMAND/clear.py
@@ -6,7 +6,7 @@ from KuroAI.KUROMAIN.DATABASE import *
 
 
 @bot.on_message(filters.command("clear_orders", prefixes=HANDLERS) & filters.user(SUDO_USERS))
-async def clear_all_orders(_, message: Message):
+async def clear_all_orders(client, message):
     await order_col.delete_many({})
     await pending_col.delete_many({})
     await completed_col.delete_many({})
